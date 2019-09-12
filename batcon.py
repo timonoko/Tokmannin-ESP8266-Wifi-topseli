62c62cc2f03bc8817f482237d92fa5c80fb20b4b
Timo Noko <timonoko@users.noreply.github.com>
2019-09-12 11:41:27 +0300

    Termux battery charger

---
+++ batcon.py
@@ -0,0 +1,26 @@
+# Termux battery charger controller.
+# The wireless charger is connected  to a Wifi-plug
+
+# This starts charging below %70 and stops charging above %80
+
+
+import os,json,time
+
+os.system("termux-wake-lock")
+while True:
+  a=json.loads(os.popen("termux-battery-status").read())
+  print a
+  p=a['percentage']
+  s=a['status']
+  if p<70 and s=="DISCHARGING":
+    os.system("termux-tts-speak battery percentage "+str(p))
+    os.system("termux-tts-speak CHARGING")
+    os.system('wget 192.168.1.61/5/on')
+    os.remove("on")
+  if p>80 and s=="CHARGING":
+    os.system("termux-tts-speak battery percentage "+str(p))
+    os.system("termux-tts-speak UNPLUGGING")
+    os.system('wget 192.168.1.61/5/off')
+    os.remove("off")
+  time.sleep(300)
+
