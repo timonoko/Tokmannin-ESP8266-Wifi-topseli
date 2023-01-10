8c1270edc83eb9163878d5c4251d428a9d0bfb03
Timo Noko <timonoko@users.noreply.github.com>
2023-01-10 12:45:46 +0200

    python vahtikoira

---
+++ boot.py
@@ -0,0 +1,58 @@
+import time
+from machine import Pin
+
+relayON =Pin(5, Pin.OUT)  
+relayOFF =Pin(12, Pin.OUT)  
+relayON.value(1)
+
+SininenLedi = Pin(4, Pin.OUT)  
+for x in range(100):
+    SininenLedi.value(1)
+    time.sleep(0.2)
+    SininenLedi.value(0)
+    time.sleep(0.2)
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    while not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('Jorpakko', 'Juhannusyona')
+        while not sta_if.isconnected():
+            time.sleep(10)
+    print('IF network config:', sta_if.ifconfig())
+
+do_connect() 
+
+def do_not_connect():
+    import network
+    ap_if = network.WLAN(network.AP_IF)
+    print('AP network config:', ap_if.ifconfig())
+    ap_if.active(False)
+    print('AP network config:', ap_if.ifconfig())
+    
+import gc
+gc.collect()
+
+import esp
+esp.osdebug(None)
+
+import os
+print(os.listdir())
+
+def ls():
+    print(os.listdir())
+
+import webrepl
+webrepl.start()
+
+do_not_connect()
+
+import vahtikoira
+
+
+
+
+
+
